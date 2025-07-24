f8f4212128b8a511c104a3bccce90d3248500954
Abbas <abbas.code786@gmail.com>
2025-07-24 18:20:14 +0530

    data type modifiers

--- Lecture7BitwiseOperator/code.cpp
+++ Lecture7BitwiseOperator/code.cpp
@@ -55,4 +55,23 @@ int main(){
     // the precedence 
     // cout << (8+10-5*4) << endl; // -2
 
+    // Scope (local vs Global)
+    // the reach of the variable
+    // scope and global
+
+
+    // Data Type modifiers
+    // can extend of minimize the capacity of the data type
+    // the size of data type can vary system to system
+    // In some system int can be allocated 2 bytes of storage
+    cout << sizeof(int) << endl; // 4 bytes
+    cout << sizeof(long int) << endl; // 4 bytes
+    cout << sizeof(short int) << endl; // 2 bytes
+    cout << sizeof(long long int) << endl; // 8 bytes
+    cout << sizeof(signed int) << endl; // 4 bytes, by default it is signed, meaning it can store both +ve & -ve values
+    cout << sizeof(unsigned int) << endl; // 4 bytes, can only store +ve values
+    
 }
+
+// 1 - Figure out if a number is power of 2 (without any loop)
+// 2 - WAF to reverse an integer n. if int n = 125, ans = 521
