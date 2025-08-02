392088e7bc904ff2142bb8f2d501114a612694e7
Abbas <abbas.code786@gmail.com>
2025-08-02 10:30:57 +0530

    Kadane's Algorithm

--- Lecture10Kadane/code.cpp
+++ Lecture10Kadane/code.cpp
@@ -3,7 +3,24 @@ Maximum subarray sum
 */
 
 #include <iostream>
+#include <vector>
 using namespace std;
+
+ // kadane's Algorithm
+    // it says add element one by one, if currSum < 0 --> currSum = 0, iterate further
+    int maxSubArray(vector<int>& nums){
+        int currSum = 0, maxSum = INT16_MIN;
+
+        for(int val : nums){
+            currSum += val;
+            maxSum = max(currSum, maxSum);
+            if(currSum < 0){
+                currSum = 0;
+            }
+        }
+
+        return maxSum;
+    }
 
 int main(){
     int n = 5;
@@ -21,16 +38,18 @@ int main(){
     // }
 
     // brute force approach to calculate maximum subarray
-    int maxSum = INT_FAST8_MIN;
-    for(int i=0; i<n; i++){
-        int curSum = 0;
-        for(int j=i; j<n; j++){
-            curSum += arr[j];
-            maxSum = max(curSum, maxSum);
-        }
-    }
-    cout << maxSum << endl;
+    // int maxSum = INT_FAST8_MIN;
+    // for(int i=0; i<n; i++){
+    //     int curSum = 0;
+    //     for(int j=i; j<n; j++){
+    //         curSum += arr[j];
+    //         maxSum = max(curSum, maxSum);
+    //     }
+    // }
+    // cout << maxSum << endl;
 
-    for(int st=0; st<n; st++)
+    vector<int> nums = {3, -4, 5, 4, -1, 7, -8};
+    cout << maxSubArray(nums) << endl;
+   
     return 0;
 }
